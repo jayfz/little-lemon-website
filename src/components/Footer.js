c14eca7231cd1f64bf1247a2183de2dc92309869
Johann <johann@helium>
2023-03-20 16:54:02 -0500

    semantic layout added

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return  (<footer><nav><ul>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>email</li>
+        </ul></nav></footer>)
+}
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import Logo from '../images/logo.png'
+
+
+export default function Header (){
+    return  <header> <img src={Logo} alt="a little lemon" /> </header>
+}
